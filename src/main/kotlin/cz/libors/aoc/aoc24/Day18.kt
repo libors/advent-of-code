e40640d7423dff1ffdadcd906df2de591e3c4657
Libor Šubčík <libor.subcik@mavenir.com>
2024-12-18 07:23:25 +0100

    2024 day18 bisection (save 1 second)

--- src/main/kotlin/cz/libors/aoc/aoc24/Day18.kt
+++ src/main/kotlin/cz/libors/aoc/aoc24/Day18.kt
@@ -9,7 +9,6 @@ object Day18 {
     fun main(args: Array<String>) {
         val input = readToLines("input18.txt").map { it.findInts() }.map { Point(it[0], it[1]) }
         val box = Pair(Point(0, 0), Point(70, 70))
-
         println(task1(box, input))
         println(task2(box, input).let { it.x.toString() + "," + it.y.toString() })
     }
@@ -17,14 +16,15 @@ object Day18 {
     private fun task1(box: Box, input: List<Point>) = shortestPath(box, input.take(1024).toSet()).getScore()
 
     private fun task2(box: Box, input: List<Point>): Point {
-        for (i in 1025 .. input.size) {
-            val took = input.take(i)
-            val corrupted = took.toSet()
-            if (!shortestPath(box, corrupted).hasPath()) return input[i - 1]
+        var left = 1024
+        var right = input.size
+        while (right - left > 1) {
+            val middle = (left + right) / 2
+            if (shortestPath(box, input.take(middle).toSet()).hasPath()) left = middle else right = middle
         }
-        throw IllegalStateException("Always possible to get through")
+        return input[right - 1]
     }
 
-    private fun shortestPath(box: Box, corrupted: Set<Point>) = bfs(box.first,
-        endFn = { it == box.second }) { it.neighbours().filter { n -> box.contains(n) && !corrupted.contains(n) } }
+    private fun shortestPath(box: Box, corrupted: Set<Point>) = bfs(box.first, endFn = { it == box.second },
+        neighboursFn = { it.neighbours().filter { n -> box.contains(n) && !corrupted.contains(n) } })
 }
